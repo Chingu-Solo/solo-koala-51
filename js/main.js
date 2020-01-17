9bb3ed6580e2723b88ba1fcdf5a0265f5776f9ac
jmoralesg <johanamgff@gmail.com>
2020-01-17 20:33:00 +1100

    add js file with the functionality of the font-size slider

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let slider = document.querySelector("#range");
+let output = document.querySelector("#font-size option");
+
+output.innerHTML = slider.value + "px";
+
+slider.oninput = function() {
+  output.innerHTML = this.value + "px";
+};
+
+slider.addEventListener("mousemove", () => {
+  let x = slider.value;
+  let color =
+    "linear-gradient(90deg, rgb(223, 42, 42)" +
+    x +
+    "%, rgb(255,212,211)" +
+    x +
+    "%)";
+  slider.style.background = color;
+});
+
+console.log("hello");
