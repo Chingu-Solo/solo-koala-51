5a440e40d946cb7ddaf707a74a964b56cffe7082
jmoralesg <johanamgff@gmail.com>
2020-02-16 23:07:23 +1100

    Responsive nav functionality

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
-let slider = document.querySelector("#slider");
-let font = document.querySelector("#font-size");
-let fontSize = document.querySelector("#font-size option");
+// Slider Functionality
 
-slider.oninput = function() {
-  fontSize.innerHTML = this.value + "px";
+let $slider = document.querySelector("#slider");
+let $font = document.querySelector("#font-size");
+let $fontSize = document.querySelector("#font-size option");
+
+$slider.oninput = function() {
+  $fontSize.innerHTML = this.value + "px";
 };
 
-font.onchange = () => {
-  if (font.value !== "8px") {
-    let fontOption = font.value;
-    slider.value = fontOption;
+$font.onchange = () => {
+  if ($font.value !== "8px") {
+    let $fontOption = $font.value;
+    $slider.value = $fontOption;
   }
   updateSlider();
 };
 
-slider.addEventListener("mousemove", () => {
+$slider.addEventListener("mousemove", () => {
   updateSlider();
 });
 
 function updateSlider() {
-  let fontValue = slider.value;
+  let fontValue = $slider.value;
   let percentage = (fontValue / 300) * 100;
   let color =
     "linear-gradient(90deg, rgb(223, 42, 42)" +
@@ -27,5 +29,34 @@ function updateSlider() {
     "%, rgb(255,212,211)" +
     percentage +
     "%)";
-  slider.style.background = color;
+  $slider.style.background = color;
+}
+
+// Responsive navigation
+const smallScreens = window.matchMedia("screen and (max-width: 661px)");
+const selectElement = element => document.querySelector(element);
+const $body = selectElement("body");
+const $headerNav = selectElement(".header-nav");
+const $burger = selectElement(".header-menu-btn");
+
+smallScreens.addListener(validation);
+
+function validation(event) {
+  if (event.matches) {
+    $burger.addEventListener("click", hideShow);
+  }
+}
+
+validation(smallScreens);
+
+function hideShow() {
+  $body.classList.toggle("is-active");
+  $headerNav.classList.toggle("is-active");
+
+  window.onclick = event => {
+    if (event.target.className === "is-active") {
+      $body.classList.remove("is-active");
+      $headerNav.classList.remove("is-active");
+    }
+  };
 }
